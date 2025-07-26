ebcb0c031f200af4bea6149a010bd2cd0edeba5d
Matb24 <matiasbch2408@gmail.com>
2025-07-26 17:21:46 -0500

    ajuste interfaz grafica

--- UI.py
+++ UI.py
@@ -13,23 +13,27 @@ class OrganizadorApp:
         self.label = tk.Label(master, text="Organizador de Archivos por Extensión", font=("Arial", 14))
         self.label.pack(pady=10)
 
+        self.texto = tk.Text(master, height=10, width=45)
+        self.texto.pack(pady=5)
+        self.texto.config(state=tk.DISABLED)
+        
         self.btn_seleccionar = tk.Button(master, text="Seleccionar Carpeta", command=self.seleccionar_carpeta)
         self.btn_seleccionar.pack(pady=5)
 
-        self.btn_resumen = tk.Button(master, text="Ver Resumen", command=self.ver_resumen)
+        self.btn_resumen = tk.Button(master, text="Analizar", command=self.ver_resumen)
         self.btn_resumen.pack(pady=5)
+        self.btn_resumen.config(state=tk.DISABLED)
 
-        self.btn_ordenar = tk.Button(master, text="Ordenar Archivos", command=self.ordenar_archivos)
+        self.btn_ordenar = tk.Button(master, text="Ordenar", command=self.ordenar_archivos)
         self.btn_ordenar.pack(pady=5)
-
-        self.btn_salir = tk.Button(master, text="Salir", command=master.quit)
-        self.btn_salir.pack(pady=20)
+        self.btn_ordenar.config(state=tk.DISABLED)
 
     def seleccionar_carpeta(self):
         ruta = filedialog.askdirectory(title="Selecciona una carpeta")
         if ruta:
             self.ruta = ruta
             messagebox.showinfo("Carpeta seleccionada", f"Ruta:\n{ruta}")
+            self.btn_resumen.config(state=tk.NORMAL)
         else:
             messagebox.showwarning("Sin selección", "No seleccionaste ninguna carpeta.")
 
@@ -39,6 +43,11 @@ class OrganizadorApp:
             return
         contador = contar_extensiones(self.ruta)
         resumen = mostrar_resumen(contador)
+        self.texto.config(state=tk.NORMAL)
+        self.texto.delete(1.0, tk.END)
+        self.texto.insert(tk.END, resumen)
+        self.texto.config(state=tk.DISABLED)
+        self.btn_ordenar.config(state=tk.NORMAL)
         messagebox.showinfo("Resumen", resumen)
 
     def ordenar_archivos(self):
